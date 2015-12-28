06dfaeaa6444366eb474fa474433b62e0b2deff4
Cherif BOUCHELAGHEM <bouchlaghem@allegorie.tv>
2015-12-28 14:37:34 +0100

    backend module

---
+++ protected/backend/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\backend;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\backend\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
